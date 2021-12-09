ca35f8b4cc10d4acc1397f4d0b6eb8511b9f07c8
keperkjr <keperkjr@yahoo.com>
2021-12-09 15:27:55 -0800

    Added employee table component with sample data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeeTableComponent } from './employee-table/employee-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeTableComponent
   ],
   imports: [
     BrowserModule
