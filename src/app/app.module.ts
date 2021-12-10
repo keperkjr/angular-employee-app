28942a127da1498b18b316f059de56ea392b6259
keperkjr <keperkjr@yahoo.com>
2021-12-09 16:15:32 -0800

    Added two way binding to the employee form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';    
 
 import { AppComponent } from './app.component';
 import { EmployeeTableComponent } from './employee-table/employee-table.component';
@@ -12,7 +13,8 @@ import { EmployeeFormComponent } from './employee-form/employee-form.component';
     EmployeeFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
